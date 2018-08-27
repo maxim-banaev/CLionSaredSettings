405ccd60313f3bcfa0b089dcffb072691c6e2818
maxim <Maxim.Banaev@jetbrains.com>
2018-08-27 17:22:22 +0300

    CL-183.1909 <maxim@DESKTOP-Q31560S Create ide.general.xml, git.xml, project.default.xml, laf.xml, other.xml, C Source File.c, Default.xml, cpp.toolchains.xml, yarn_lock.xml, file.template.settings.xml, ui.lnf.xml, wsl.distributions.xml, editor.codeinsight.xml, colors.scheme.xml, github_settings.xml, webServers.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/C Source File.c
@@ -0,0 +1,9 @@
+/**
+ * @file ${NAME}.c
+ * @author $!{AUTHOR_NAME} <$!{AUTHOR_EMAIL}>
+ * @brief TODO: Description for ${NAME}
+ */
+#if (${HEADER_FILENAME})
+#[[#include]]# "${HEADER_FILENAME}"
+#end
+#[[$bla$ $END$]]#
